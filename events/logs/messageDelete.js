f4db8688a5a2f910be4b752d91faf23b4382f8f5
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-09 13:31:10 -0500

    Create messageDelete.js

---
+++ events/logs/messageDelete.js
@@ -0,0 +1,15 @@
+module.exports = async (bot, message) => { 
+  	if (message.author.bot) return;
+const logchannel = message.guild.channels.find(channel => channel.name === "omega-logs");
+const user = message.author
+if(!logchannel) return
+let deleteEmbed = new Discord.RichEmbed()
+  .setTitle("⚠️ A message was deleted!")
+  .addField("User", user.tag)
+.addField("Content", "```" + message.content + "```")
+.addField("Channel", message.channel.name)
+.setFooter(`MessageID: ${message.id} | AuthorID: ${user.id}`, bot.user.avatarURL)
+.setColor("#FF0000")
+.setThumbnail(user.avatarURL)
+logchannel.send(deleteEmbed)
+}
