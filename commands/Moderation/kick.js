dc519773c09d88c9f4e2528a74b276d07c77abc5
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-09 18:18:53 -0500

    Update and rename kick to kick.js

---
+++ commands/Moderation/kick.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const Discord = require('discord.js')
+const prefix = '^'
+module.exports = {
+  name: 'kick',
+  description: 'Kicks the targeted member from your server.',
+  category: 'Moderation',
+  aliases: ['k', 'remove'],
+  
+  run:async (bot, message, args) => {
+   
+    }
+}
