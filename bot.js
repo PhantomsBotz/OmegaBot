2cb3416af38ce659120c1ff63d868dff6e0d5b84
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-09 00:36:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ bot.aliases = new Discord.Collection()
     const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
     
     for (let file of commands) {
-      let pull = require(`../commands/${dir}/${file}`);
+      let pull = require(`./commands/${dir}/${file}`);
     
       if (pull.name) {
         client.commands.set(pull.name, pull);
