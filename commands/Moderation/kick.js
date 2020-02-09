b3e35a0827cc5cabaa940ced676b94d132affed7
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-09 18:01:50 -0500

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -8,21 +8,24 @@ module.exports = {
   aliases: ['kick'],
   
   run:async (bot, message, args) => {
-    if (!message.member.hasPermission(["KICK_MEMBERS"])) return message.channel.send("You do not have KICK_MEMBERS permissions.")
+  if (!message.member.hasPermission(["KICK_MEMBERS"])) return;
     let member = message.mentions.members.first();
-    if (!member) return message.channel.send("You need to mention someone to ban.")
+    if (!member) return message.channel.send("You need to mention someone to kick.")
     let reason = args.slice(1).join(' ');
-    
-        if (!reason) {
+    if (!reason) {
         if (member.displayName) {
-            member.ban().then(message.channel.send(`Okay, just banned ${member.displayName}.`))
+            member.kick().then(message.channel.send(`Okay, just kicked ${member.displayName}.`))
         } else {
-            member.ban().then(message.channel.send(`Okay, just banned ${member.user.username}.`))
-        } else {
-        if (member.displayName) {
-            member.ban(reason).then(message.channel.send(`Okay, just banned ${member.displayName}.`))
-        } else {
-            member.ban(reason).then(message.channel.send(`Okay, just banned ${member.user.username}.`))
+            member.kick().then(message.channel.send(`Okay, just kicked ${member.user.username}.`))
         }
+    } else {
+        if (member.displayName) {
+            member.kick(reason).then(message.channel.send(`Okay, just kicked ${member.displayName}.`))
+        } else {
+            member.kick(reason).then(message.channel.send(`Okay, just kicked ${member.user.username}.`))
+        }
+    }
+}
+
     }
 }
