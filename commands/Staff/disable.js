b7a2ea02521817998d0dbd5fa7606b130ef08465
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-10 17:49:27 -0500

    Update disable.js

--- commands/Staff/disable.js
+++ commands/Staff/disable.js
@@ -7,11 +7,9 @@ module.exports = {
   description: 'Disables a command or entire selection of commands',
   category: 'Staff',
   run: async (bot, message, args) => {
-  const dir = bot.commands.filter(c => c.category === category)
-  const capitalise = category.slice(0, 1).toUpperCase() + category.slice(1)
-  message.channel.send(capitalise)
-   // let isDisabled = await db.fetch(`Disabled_${message.guild.id}_${args[0]`)
-   // if (isDisabled == true) return message.channel.send('That command is already disabled!')
-   // else if (
+     let command = bot.commands.get(args[0].toLowerCase())
+    let isDisabled = await db.fetch(`Disabled_${message.guild.id}_${args[0]`);
+    if (isDisabled == true) return message.channel.send('That command is already disabled!');
+    if (!command) return;
   }
 }
