c3ef27ff7d58bf63bf645196db318a6e17b7afc3
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-09 21:48:36 -0500

    Create userstatus.js

---
+++ commands/Information/userstatus.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const Discord = require('discord.js')
+const prefix = '^'
+module.exports = {
+  name: 'userstatus',
+  description: 'Checks your basic status.',
+  category: 'Information',
+  
+  run:async (bot, message, args) => {
+      let isDeveloper;
+      let isPremium;
+      let isPartner;
+      
+      if(message.member.roles.find(r => r.name === "Premium Member")) {
+        isPremium = true
+      } else {
+        isPremium = false
+      }
+      if(message.author.id == "513103852409716736" || message.author.id == "456641711486009355") {
+        isDeveloper = true
+      } else {
+        isDeveloper = false
+      }
+      isPartner = false
+      
+      let em = new Discord.RichEmbed()
+      .setTitle("User Status")
+      .addField("Is Premium User": isPremium)
+      .addField("Is Developer": isDeveloper)
+      .addField("Is Partner": isPartner)
+      if(isPremium = true) {
+      embed.setColor("GOLD")
+      } else {
+      embed.setColor("#5595A6")
+      message.channel.send({embed: em})
+      }
+      
+    }
+}
