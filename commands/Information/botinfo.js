6efa9799a6a5b3311f3053cfce5d4d32051e8938
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-09 22:55:11 -0500

    Update botinfo.js

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -2,26 +2,28 @@ const fs = require('fs')
 const Discord = require('discord.js')
 const prefix = '^'
 module.exports = {
-  name: 'botinfo',
-  description: 'Shows the bot\'s info.',
-  category: 'Information',
-  
-  run:async (bot, message, args) => {
-     let ping = Math.floor(bot.ping)
-     let status = bot.user.status
-     let em = new Discord.RichEmbed()
-     .setTitle("Bot Information")
-     .addField("Ping", ping)
-     .addField("Prefix", "^")
-     .addField("Status", status)
-     .addField("Nickname", bot.user.displayName)
-     if (status = "online") {
-      em.setColor("GREEN")
-     } else if (status = "idle") {
-      em.setColor("GOLD")
-     } else if (status = "dnd") {
-      em.setColor("RED")
-     }
-     message.channel.send({embed: em})
+    name: 'botinfo',
+    description: 'Shows the bot\'s info.',
+    category: 'Information',
+
+    run: async (bot, message, args) => {
+        let ping = Math.floor(bot.ping)
+        let status = bot.user.status
+        let em = new Discord.RichEmbed()
+            .setTitle("Bot Information")
+            .addField("Ping", ping)
+            .addField("Prefix", "^")
+            .addField("Status", status)
+            .addField("Nickname", bot.user.displayName)
+        if (status = "online") {
+            em.setColor("GREEN")
+        } else if (status = "idle") {
+            em.setColor("GOLD")
+        } else if (status = "dnd") {
+            em.setColor("RED")
+        }
+        message.channel.send({
+            embed: em
+        })
     }
 }
