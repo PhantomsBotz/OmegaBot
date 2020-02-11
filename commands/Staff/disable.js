7560ebf3d465dcbad789d4bd1fff7b239e6ba71e
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-11 18:06:39 -0500

    Update disable.js

--- commands/Staff/disable.js
+++ commands/Staff/disable.js
@@ -20,7 +20,6 @@ module.exports = {
     
     
      let command = bot.commands.get(args[0].toLowerCase());
-     console.log(command.name);
     if (args[0] == 'help' || args[0] == 'disable' || args[0] == 'enable' || args[0] == 'support') return message.channel.send(':x: That command cannot be disabled.');
     let isDisabled = await db.fetch(`Disabled_${message.guild.id}_${args[0]}`);
     if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
