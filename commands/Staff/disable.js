3b90a23d6d0f1428afc915e1f87e93a0cf5555a0
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-11 15:31:35 -0500

    Update disable.js

--- commands/Staff/disable.js
+++ commands/Staff/disable.js
@@ -2,15 +2,16 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const db = require('quick.db');
 const prefix = '^';
-const embed = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setAuthor(`${message.guild.me.displayName}`, message.guild.iconURL)
-            .setThumbnail(bot.user.displayAvatarURL);
+
 module.exports = {
   name: 'disable',
   description: 'Disables a command or entire selection of commands',
   category: 'Staff',
   run: async (bot, message, args) => {
+        const embed = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(`${message.guild.me.displayName}`, message.guild.iconURL)
+            .setThumbnail(bot.user.displayAvatarURL);      
      let command = bot.commands.get(args[0].toLowerCase())
     let isDisabled = await db.fetch(`Disabled_${message.guild.id}_${args[0]}`);
     if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
