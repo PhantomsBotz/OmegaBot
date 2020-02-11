71aad935637fa08d7fa7f946f8333a079851bfb0
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-11 15:51:50 -0500

    Create enable.js

---
+++ commands/Staff/enable.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const db = require('quick.db');
+const prefix = '^';
+
+module.exports = {
+  name: 'enable',
+  description: 'Enable a command or entire selection of commands',
+  category: 'Staff',
+  run: async (bot, message, args) => {
+    if (message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Error: Missing permissions')
+    let success = new Discord.RichEmbed()
+    .setTitle('Success!')
+    .setDescription(`Successfully enabled the command ${args[0]}.`)
+    .setColor('#5595A6');
+        const embed = new Discord.RichEmbed()
+            .setColor('#5595A6')
+            .setAuthor(`${message.guild.me.displayName}`, message.guild.iconURL)
+            .setThumbnail(bot.user.displayAvatarURL);    
+    
+    
+     let command = bot.commands.get(args[0].toLowerCase());
+     console.log(command.name);
+    let isDisabled = await db.fetch(`Disabled_${message.guild.id}_${args[0]}`);
+    if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
+    if (isDisabled == null) isDisabled = false
+    if (isDisabled == false) return message.channel.send('That command is already enabled!');
+    else {
+     db.set(`Disabled_${message.guild.id}_${args[0]}`, false)    
+      message.channel.send(success)
+    }
+  }
+}
