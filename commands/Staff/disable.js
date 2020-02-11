ce282b2625381cf4a4a5cc3013fc89816937fa17
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-10 19:32:50 -0500

    Update and rename disable to disable.js

--- commands/Staff/disable.js
+++ commands/Staff/disable.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const db = require('quick.db');
 const prefix = '^';
+const embed = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(`${message.guild.me.displayName}`, message.guild.iconURL)
+            .setThumbnail(bot.user.displayAvatarURL);
 module.exports = {
   name: 'disable',
   description: 'Disables a command or entire selection of commands',
@@ -10,6 +14,6 @@ module.exports = {
      let command = bot.commands.get(args[0].toLowerCase())
     let isDisabled = await db.fetch(`Disabled_${message.guild.id}_${args[0]`);
     if (isDisabled == true) return message.channel.send('That command is already disabled!');
-    if (!command) return;
+    if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
   }
 }
