dd9d7f104ef920b50375b40ccd863db212c183df
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-12 17:03:59 -0500

    Update userstatus.js

--- commands/Information/userstatus.js
+++ commands/Information/userstatus.js
@@ -9,31 +9,28 @@ module.exports = {
   category: 'Information',
   
   run:async (bot, message, args) => {
+      const MySQL = require("mysql")
+var con = MySQL.createConnection({
+    "host": "remotemysql.com",
+    "user": "qOnmCFWJtY",
+    "database": "qOnmCFWJtY",
+    "password": process.env.SQLPASS
+})
+   
     let isDisabled = await db.fetch(`Disabled_${message.guild.id}_userstatus`);
     if (isDisabled == true) return;
       let isDeveloper;
       let isPremium;
       let isPartner;
-      
-      if(message.member.roles.find(r => r.name === "Premium User")) {
-        isPremium = "True"
-        console.log("Premium = True")
-      } else {
-        isPremium = "False"
-        console.log("Premium = False")
-      }
-      if(message.author.id == "513103852409716736" || message.author.id == "456641711486009355") {
-        isDeveloper = "True"
-        console.log("Developer = True")
-      } else {
-        isDeveloper = "False"
-        console.log("Developer = False")
-      }
-      if(message.member.roles.find(r => r.name === "Partnerships")) {
-       isPartner = "True"
-      } else {
-       isPartner = "False"
-      }
+   
+   con.query(`SELECT * FROM user WHERE userid = ${message.author.id}`, (err, rows) => {
+    let premiumCheck = rows[0].premium
+    let partnerCheck; // Not Implimented Yet.
+    
+    if premiumCheck = 1 return isPremium = "True"
+    
+   })
+     
       let em = new Discord.RichEmbed()
       .setTitle("User Status")
       .addField("Is Premium User", isPremium)
