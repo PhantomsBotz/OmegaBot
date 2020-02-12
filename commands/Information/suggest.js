f50f6cb202ad2d3fa99c2d8df31028f9d201ef8b
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-11 20:06:54 -0500

    Create suggest.js

---
+++ commands/Information/suggest.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const Discord = require('discord.js')
+const prefix = '^'
+module.exports = {
+    name: 'suggest',
+    description: 'Allows you to suggest commands and other features to our developer team.',
+    category: 'Information',
+
+    run: async (bot, message, args) => {
+      let Channel = "676953329536073759"
+      let suggest = args.join(" ");
+      let em = new Discord.RichEmbed()
+      .setTitle("Suggestion")
+      .setDescription(`This was sent by ${author.username}`)
+      .addField("Suggestion: ", suggest)
+      .setColor("#5595A6")
+      
+      bot.channels.get(Channel).send({embed: em})
+    }
+}
