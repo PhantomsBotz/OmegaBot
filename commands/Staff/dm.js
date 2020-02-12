46bae87df87c38e55553a977e248863b553de19c
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-11 20:56:06 -0500

    Create dm.js

---
+++ commands/Staff/dm.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const Discord = require('discord.js')
+const prefix = '^'
+module.exports = {
+    name: 'dm',
+    description: 'Allows staff to remotly contact the user.',
+    category: 'Staff',
+
+    run: async (bot, message, args) => {
+      let targetid = args(1)
+      let Message = args.join(" ").splice(1)
+      
+      let targetMessage = new Discord.RichEmbed()
+      .setTitle("Message from my Developer Team")
+      .setDescription(`This was sent by ${message.author.username}`)
+      .addField("Message: ", Message)
+      .setColor("#5595a6")
+      
+      let response = new Discord.RichEmbed()
+      .setTitle("Message Sent.")
+      .setColor("#5595a6")
+    
+    if(message.author.id == "513103852409716736" || message.author.id == "456641711486009355") {
+        bot.users.get(targetid).send({embed: targetMessage}).then(message.channel.send({embed: response}))
+      }
+    }
+}
