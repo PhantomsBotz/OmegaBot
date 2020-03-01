6f558c01efac04869ecf07b83edc918dda22cc97
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-29 21:33:33 -0500

    Update disable.js -- Replace quick.db with mysql

--- commands/Staff/disable.js
+++ commands/Staff/disable.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const db = require('quick.db');
 const prefix = '^';
-
 module.exports = {
   name: 'disable',
   description: 'Disables a command or entire selection of commands',
@@ -15,15 +14,22 @@ var con = MySQL.createConnection({
     "database": "qOnmCFWJtY",
     "password": process.env.SQLPASS
 })
+if (!args[0]) return message.channel.send('Invalid Usage: Please give a command to disable!')
+let command = bot.commands.get(args[0].toLowerCase());
+    if (args[0] == 'help' || args[0] == 'disable' || args[0] == 'enable' || args[0] == 'support') return message.channel.send(':x: That command cannot be disabled.');
+    if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
+     if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('Error: Missing permissions')
+    
     con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT * FROM server", function (err, result, fields) {
+  con.query(`SELECT ${args[0]} FROM server`, function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
-  });
-});
-    if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('Error: Missing permissions')
-    let success = new Discord.RichEmbed()
+    if (result === '1') return message.channel.send('That command is disabled already!')
+    else {
+      let sql = `UPDATE server SET ${args[0]} = '1' WHERE guildid = ${message.guild.id}`;
+      
+      con.query(sql, console.log)
+      let success = new Discord.RichEmbed()
     .setTitle('Success!')
     .setDescription(`Successfully disabled the command ${args[0]}.`)
     .setColor('#5595A6');
@@ -31,15 +37,12 @@ var con = MySQL.createConnection({
             .setColor('#5595A6')
             .setAuthor(`${message.guild.me.displayName}`, message.guild.iconURL)
             .setThumbnail(bot.user.displayAvatarURL);    
-    
-    
-     let command = bot.commands.get(args[0].toLowerCase());
-    if (args[0] == 'help' || args[0] == 'disable' || args[0] == 'enable' || args[0] == 'support') return message.channel.send(':x: That command cannot be disabled.');
-    if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
-    if (isDisabled == '1') return message.channel.send('That command is already disabled!');
-    else {
-     db.set(`Disabled_${message.guild.id}_${args[0]}`, true)    
       message.channel.send(success)
+    }
+  });
+});
+   
+    
     }
   }
 }
