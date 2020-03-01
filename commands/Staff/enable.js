ab6db131eddbe854e07a8862e9e8b38ef74fcbad
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-02-29 21:42:46 -0500

    Update enable.js -- quick.db to mySQL

--- commands/Staff/enable.js
+++ commands/Staff/enable.js
@@ -2,14 +2,34 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const db = require('quick.db');
 const prefix = '^';
-
 module.exports = {
   name: 'enable',
-  description: 'Enable a command or entire selection of commands',
+  description: 'Enables a command or entire selection of commands',
   category: 'Staff',
   run: async (bot, message, args) => {
- if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('Error: Missing permissions')
-    let success = new Discord.RichEmbed()
+       const MySQL = require("mysql")
+var con = MySQL.createConnection({
+    "host": "remotemysql.com",
+    "user": "qOnmCFWJtY",
+    "database": "qOnmCFWJtY",
+    "password": process.env.SQLPASS
+})
+if (!args[0]) return message.channel.send('Invalid Usage: Please give a command to disable!')
+let command = bot.commands.get(args[0].toLowerCase());
+    if (args[0] == 'help' || args[0] == 'disable' || args[0] == 'enable' || args[0] == 'support') return message.channel.send(':x: That command cannot be disabled.');
+    if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
+     if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('Error: Missing permissions')
+    
+    con.connect(function(err) {
+  if (err) throw err;
+  con.query(`SELECT ${args[0]} FROM server`, function (err, result, fields) {
+    if (err) throw err;
+    if (result === '0') return message.channel.send('That command is enabled already!')
+    else {
+      let sql = `UPDATE server SET ${args[0]} = '0' WHERE guildid = ${message.guild.id}`;
+      
+      con.query(sql, console.log)
+      let success = new Discord.RichEmbed()
     .setTitle('Success!')
     .setDescription(`Successfully enabled the command ${args[0]}.`)
     .setColor('#5595A6');
@@ -17,17 +37,12 @@ module.exports = {
             .setColor('#5595A6')
             .setAuthor(`${message.guild.me.displayName}`, message.guild.iconURL)
             .setThumbnail(bot.user.displayAvatarURL);    
-    
-    
-     let command = bot.commands.get(args[0].toLowerCase());
-    let isDisabled = await db.fetch(`Disabled_${message.guild.id}_${args[0]}`);
-        if (args[0] == 'help' || args[0] == 'disable' || args[0] == 'enable' || args[0] == 'support') return message.channel.send(':x: That command cannot be disabled.');
-    if (!command) return message.channel.send(embed.setTitle("Invalid Command.").setDescription(`Do \`${prefix}help\` for the list of the commands.`))
-    if (isDisabled == null) isDisabled = false
-    if (isDisabled == false) return message.channel.send('That command is already enabled!');
-    else {
-     db.set(`Disabled_${message.guild.id}_${args[0]}`, false)    
       message.channel.send(success)
+    }
+  });
+});
+   
+    
     }
   }
 }
