de6e447c6ba1228a2d3039cec62d973451a966a5
NewoMadness <52988707+NewoMadness@users.noreply.github.com>
2020-04-02 21:15:32 -0400

    Update userstatus.js

--- commands/Information/userstatus.js
+++ commands/Information/userstatus.js
@@ -9,23 +9,26 @@ module.exports = {
   category: 'Information',
   
   run:async (bot, message, args) => {
+   let isDeveloper;
+   let isPremium;
+   let isPartner;
 let status = bot.getUserStatus.get(message.author.id);
  
     let premiumCheck = status.premium;
     let partnerCheck = status.partner;
     if (message.author.id == '456641711486009355' || message.author.id == '513103852409716736'){
-     let isDeveloper = 'True';
-    } else let isDeveloper = 'False'
+     isDeveloper = 'True';
+    } else isDeveloper = 'False'
    
    
     if(premiumCheck = 1) {
-     let isPremium = "True"
-    } else let isPremium = "False"
+     isPremium = "True"
+    } else isPremium = "False"
     
     
     if(partnerCheck = 1) {
-     let isPartner = "True"
-    } else let isPartner = "False"
+     isPartner = "True"
+    } else isPartner = "False"
      
       let em = new Discord.RichEmbed()
       .setTitle("User Status")
